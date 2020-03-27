2100d1efe1da8e78bb729f192da053d35baf50ab
basir12 <abobaidy@gmail.com>
2020-03-26 21:27:40 -0700

    update the assignments (1)

--- Junit_OnlineClass/src/test/java/com/Assigments/JunitAssertions.java
+++ Junit_OnlineClass/src/test/java/com/Assigments/JunitAssertions.java
@@ -1,26 +1,43 @@
 package com.Assigments;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class JunitAssertions {
-	
-	
-	
+
+	@Before
 	public static void beforeClass() {
-		
+
 	}
-	
+
+	@After
 	public void after() {
-		
+
 	}
-	
+
+	@Test
 	public void assertNull() {
-		
-		
+
+		Assert.assertNull("Fail", "Exception");
+
 	}
-	
-	
-	
-	
-	
-	
+
+	@Test
+	public void asserNotNull() {
+
+		assertNotNull("Json");
+	}
+
+	@Test
+	public void asserEquals() {
+
+		assertEquals("Alert", "Alert");
+
+	}
 
 }
